ed442efb5d116f5901dcc9162014fed235389689
loco-pritesh <loco-pritesh>
2024-04-08 10:37:20 +0000

    t7 regen

--- src/components/frame-component.tsx
+++ src/components/frame-component.tsx
@@ -1,39 +1,48 @@
 import * as React from "react";
 import { FunctionComponent } from "react";
-import * as styles from "./frame-component.module.css";
+import * as styles from "./frame-component7.module.css";
 
 const FrameComponent: FunctionComponent = () => {
   return (
-    <section className={styles.newsletter}>
-      <div className={styles.rectangleParent}>
-        <div className={styles.frameChild} />
-        <div className={styles.stayInTheLoopParent}>
-          <h2 className={styles.stayInThe}>Stay in the loop</h2>
-          <div className={styles.subscribeToReceiveContainer}>
-            <p className={styles.subscribeToReceive}>
-              Subscribe to receive the latest news and updates about TDA.
-            </p>
-            <p
-              className={styles.wePromiseNot}
-            >{`We promise not to spam you! `}</p>
-          </div>
+    <div className={styles.fifoBuffer}>
+      <div className={styles.lruCache}>
+        <div className={styles.lruCacheChild} />
+        <div className={styles.linkedList}>
+          <h2 className={styles.loremIpsum}>Lorem Ipsum</h2>
+          <div
+            className={styles.loremIpsumIs}
+          >{`Lorem Ipsum is simply dummy text of the printing and typesetting industry. `}</div>
         </div>
-        <div className={styles.emailInput}>
-          <div className={styles.rectangleGroup}>
-            <div className={styles.frameItem} />
-            <div className={styles.inputBox}>
-              <div className={styles.enterEmailAddress}>
-                Enter email address
+        <div className={styles.rectangleParent}>
+          <div className={styles.frameChild} />
+          <div className={styles.priorityQueue}>
+            <h2 className={styles.stayInThe}>Stay in the loop</h2>
+            <div className={styles.subscribeToReceiveContainer}>
+              <p className={styles.subscribeToReceive}>
+                Subscribe to receive the latest news and updates about TDA.
+              </p>
+              <p
+                className={styles.wePromiseNot}
+              >{`We promise not to spam you! `}</p>
+            </div>
+          </div>
+          <div className={styles.treeMap}>
+            <div className={styles.rectangleGroup}>
+              <div className={styles.frameItem} />
+              <div className={styles.binarySearchTree}>
+                <div className={styles.enterEmailAddress}>
+                  Enter email address
+                </div>
+              </div>
+              <div className={styles.graph}>
+                <div className={styles.graphChild} />
+                <div className={styles.continue1}>Continue</div>
               </div>
             </div>
-            <button className={styles.continueButton}>
-              <div className={styles.continueButtonChild} />
-              <div className={styles.continue1}>Continue</div>
-            </button>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
